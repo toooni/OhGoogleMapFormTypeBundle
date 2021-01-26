40716079caa6e1de308b9cb7bafa8ffb0b51ecd7
Toni Rudolf <toni.rudolf@gmail.com>
2021-01-26 17:34:00 +0100

    Allow lat,lng and latitude,langitude in validator

--- Validator/Constraints/LatLngValidator.php
+++ Validator/Constraints/LatLngValidator.php
@@ -9,15 +9,18 @@ class LatLngValidator extends ConstraintValidator
 {
     public function validate($value, Constraint $constraint)
     {
-		if (!preg_match('/^[0-9\-\.]+$/', $value['latitude'], $matches) || !preg_match('/^[0-9\-\.]+$/', $value['longitude'], $matches)) {
-			 $this->context->addViolation($constraint->message, array('%latitude%' => (float)$value['latitude'], '%longitude%' => (float)$value['longitude']));
-			 return false;
-		}
-		if($value['latitude'] > 90 || $value['latitude'] < -90 || $value['longitude'] > 180 || $value['longitude'] < -180)
-		{
-			 $this->context->addViolation($constraint->message, array('%latitude%' => (float)$value['latitude'], '%longitude%' => (float)$value['longitude']));
-			 return false;
-		}
+        $latitude = isset($value['latitude']) ? $value['latitude'] : $value['lat'];
+        $longitude = isset($value['longitude']) ? $value['longitude'] : $value['lng'];
+
+        if (!preg_match('/^[0-9\-\.]+$/', $latitude, $matches) || !preg_match('/^[0-9\-\.]+$/', $longitude, $matches)) {
+            $this->context->addViolation($constraint->message, array('%latitude%' => (float)$latitude, '%longitude%' => (float)$longitude));
+            return false;
+        }
+        if($latitude > 90 || $latitude < -90 || $longitude > 180 || $longitude < -180)
+        {
+            $this->context->addViolation($constraint->message, array('%latitude%' => (float)$latitude, '%longitude%' => (float)$longitude));
+            return false;
+        }
 
         return true;
     }
